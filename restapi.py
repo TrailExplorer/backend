87b13bfa2207d6b48fdcfbfaf968a77d2877ac6e
Govardhan N <Govardhan.Narasimha@colorado.edu>
2024-03-19 20:19:00 -0600

    Added Github Actions to execute on Main Branch Push

--- restapi.py
+++ restapi.py
@@ -5,7 +5,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def helloworld():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello, World! How are you?</p>"
 
 if __name__=='__main__':
-    app.run()
+    app.run(host="0.0.0.0")
