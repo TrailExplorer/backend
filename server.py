7c9d2d9d184d9190788c73d79027d14f2d8dd7cf
Sachin Rathod <36642643+sachi097@users.noreply.github.com>
2024-04-25 23:04:47 -0600

    Feature/add statename to difficulty endpoint (#42)
    * feat: added query parameters
    
    * feat: added search functionality
    
    * feat: open-weather
    
    ---------
    
    Co-authored-by: Sachin Rathod <sachin@Sachins-MacBook-Air.local>
    Co-authored-by: Sachin Rathod <sachin@engr2-2-70-44-dhcp.int.colorado.edu>

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
-import aws_controller
+import trails_controller, open_weather_controller
 
 app=Flask(__name__)
 cors = CORS(app)
@@ -15,35 +15,40 @@ def helloworld():
 def get_trail_details():
     state_name=request.args.get('state_name')
     trail_id=request.args.get('trail_id')
-    return jsonify(aws_controller.get_trail_details(state_name, trail_id))
+    return jsonify(trails_controller.get_trail_details(state_name, trail_id))
 
 @app.route('/get-trails-by-difficulty-rating', methods=['GET'])
 @cross_origin()
 def get_trails_by_difficulty_rating():
     state_name=request.args.get('state_name')
     difficulty_rating=request.args.get('difficulty_rating')
-    return jsonify(aws_controller.get_trails_by_difficulty_rating(difficulty_rating, state_name))
+    return jsonify(trails_controller.get_trails_by_difficulty_rating(difficulty_rating, state_name))
 
 @app.route('/get-trails-by-length', methods=['GET'])
 @cross_origin()
 def get_trails_by_length():
     state_name=request.args.get('state_name')
     length=request.args.get('length')
-    return  jsonify(aws_controller.get_trails_by_length(length,state_name))
+    return  jsonify(trails_controller.get_trails_by_length(length,state_name))
 
 @app.route('/get-trails-by-rating', methods = ['GET'])
 @cross_origin()
 def get_trails_by_rating():
     state_name = request.args.get('state_name')
     rating = request.args.get('rating')
-    return jsonify(aws_controller.get_trails_by_rating(state_name, rating))
+    return jsonify(trails_controller.get_trails_by_rating(state_name, rating))
 
 @app.route('/trails-search', methods = ['GET'])
 @cross_origin()
 def trails_search():
     state_name = request.args.get('state_name')
     trail_name = request.args.get('trail_name')
-    return jsonify(aws_controller.trails_search(state_name, trail_name))
+    return jsonify(trails_controller.trails_search(state_name, trail_name))
+
+@app.route('/open-weather-api', methods = ['GET'])
+@cross_origin()
+def open_weather_api():
+    return jsonify(open_weather_controller.open_weather_api())
 
 if __name__=='__main__':
     app.run()
